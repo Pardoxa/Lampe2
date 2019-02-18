e62525625ab02ced41c4b9877503b82b3d11ff33
Yannick Feld <yannick.feld@uol.de>
2019-02-18 14:31:12 +0100

    Drawing + ColorPicker

--- app/src/main/java/com/yannick/feld/lampe2/picture.java
+++ app/src/main/java/com/yannick/feld/lampe2/picture.java
@@ -15,29 +15,29 @@ import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+
+import static android.view.MotionEvent.ACTION_DOWN;
+import static android.view.MotionEvent.ACTION_MOVE;
 
 
 public class picture extends AppCompatActivity {
 
-    private Button back_btn ;
+    private Button back_btn, chess_btn, fill;
+    private ImageButton color_btn;
     private ImageView img;
     private int[] img_viewCoords = new int[2];
     private Bitmap pixel = Bitmap.createBitmap(16,16, Bitmap.Config.ARGB_4444);
     private int minimum = 0;
+    private final int max_size = 15;
+    private int red, green, blue;
 
+    private int old_x = -1, old_y = -1;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_picture);
-        back_btn = findViewById(R.id.back_btn);
-        back_btn.setOnClickListener(v -> finish());
-        img = findViewById(R.id.img);
-
-
-
+    private void reset_chess(){
         //Get size of Screen for adjusting the hight.
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
@@ -45,10 +45,8 @@ public class picture extends AppCompatActivity {
         int width = size.x;
         int height = size.y;
         minimum = Math.min(width, height);
-        //CreateShape createShape = new CreateShape();
-        //createShape.CreateShape(minimum,4,minimum,Math.PI / 4);
+  
         Log.d("minimum", Integer.toString(minimum));
-        //img.setBackground(createShape.getShape(minimum,minimum, Color.BLUE));
 
 
         for(int x = 0 ; x < pixel.getWidth(); x++){
@@ -64,13 +62,50 @@ public class picture extends AppCompatActivity {
             }
         }
         Bitmap bitmap = Bitmap.createScaledBitmap(pixel,minimum,minimum,false);
-    //    Canvas canvas = new Canvas(bitmap);
+        //    Canvas canvas = new Canvas(bitmap);
 
         img.setImageBitmap(bitmap);
-        img.getLocationOnScreen(img_viewCoords);
-        img.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+        back_btn = findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(v -> finish());
+        img = findViewById(R.id.img);
+
+        reset_chess();
+        chess_btn = findViewById(R.id.chess_btn);
+        chess_btn.setOnClickListener(v -> reset_chess());
+
+        red = 0;
+        green = 0;
+        blue = 0;
+        color_btn = findViewById(R.id.color_btn);
+        color_btn.setOnClickListener(v ->{
+            final ColorPicker cp = new ColorPicker(picture.this, red, green, blue);
+            cp.show();
+            Button okColorBtn = cp.findViewById(R.id.okColorButton);
+            okColorBtn.setOnClickListener(v2 ->{
+                red = cp.getRed();
+                green = cp.getGreen();
+                blue = cp.getBlue();
+                color_btn.setBackgroundColor(android.graphics.Color.rgb(red, green, blue));
+                cp.dismiss();
+            });
+        });
+
+        fill = findViewById(R.id.fill_color_btn);
+        fill.setOnClickListener(v ->{
+            pixel.eraseColor(Color.rgb(red, green, blue));
+            img.setImageBitmap(Bitmap.createScaledBitmap(pixel, minimum, minimum,false));
+        });
+
+
+     //   img.getLocationOnScreen(img_viewCoords);
+        img.setOnTouchListener((v, event) ->
+             {
                 // https://stackoverflow.com/questions/11312128/get-the-touch-position-inside-the-imageview-in-android
                 int touchX = (int) event.getX();
                 int touchY = (int) event.getY();
@@ -81,17 +116,31 @@ public class picture extends AppCompatActivity {
                 imageY /= img.getHeight();
                 int x = (int) (imageX * 16);
                 int y = (int) (imageY * 16);
+                x = Math.min(x, max_size);
+                y = Math.min(y, max_size);
+                x = Math.max(0, x);
+                y = Math.max(0, y);
+                switch (event.getActionMasked()){
+                    case ACTION_DOWN:
 
-                Log.d(Integer.toString(x),Integer.toString(y));
-                pixel.setPixel(x,y,Color.BLUE);
-                runOnUiThread(() -> img.setImageBitmap(Bitmap.createScaledBitmap(pixel, minimum, minimum, false)));
-                return false;
+                        Log.d("Action", "DOWN");
+                        Log.d(Integer.toString(x),Integer.toString(y));
+                        break;
+                    case ACTION_MOVE:
+                        Log.d("Action", "Move");
+                        Log.d(Integer.toString(x),Integer.toString(y));
+                }
+                if(old_x != x || old_y != y){
+                    pixel.setPixel(x, y, android.graphics.Color.rgb(red, green, blue));
+                    old_x = x;
+                    old_y = y;
+                    img.setImageBitmap(Bitmap.createScaledBitmap(pixel, minimum, minimum, false));
+                }
+
+                return true;
             }
 
-
-        });
-
-
+        );
 
     }
 }
